ed951ba30eadc921d302391b22baeab8fb681ebd
Saiaaradhana <saiaaradhana.str@gmail.com>
2025-12-12 13:54:22 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket = "tfprodstatefile"
+    key = "main/dev/terraform.tfstate"
+    region = "us-west-2"
+    use_lockfile = true
+  }
+  required_version = "1.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+
+provider "aws" {
+  region = var.default_region
+}
